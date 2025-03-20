a5ef6839f579b096950ec59c554b0b604af95797
Voedoe-H <heermannhagen@gmail.com>
2025-03-20 14:10:19 +0100

    Fixing dimensions

--- models/model.py
+++ models/model.py
@@ -14,17 +14,23 @@ class CNNAutoencoderADModel(tf.keras.Model):
     def __init__(self):
         super(CNNAutoencoderADModel,self).__init__()
         
+        # O := Output Size
+        # I := Input Size
+        # K := Kernel Size
+        # S := Stride 
+        # O = (I-K)/S + 1
+
         # Encoder Part of the model:
         self.encoder = models.Sequential([
-            layers.Conv2D(32, (3, 3), activation='relu', padding='valid', strides=2, input_shape=input_shape),
+            layers.Conv2D(32, (3, 3), activation='relu', padding='valid', strides=2, input_shape=input_shape), # 1024x1024 -> (1024-3)/2 + 1 => 512x512x32
             layers.BatchNormalization(),
-            layers.Conv2D(64, (3, 3), activation='relu', padding='valid', strides=2),
+            layers.Conv2D(64, (3, 3), activation='relu', padding='valid', strides=2), # 512x512x3 -> (512-3)/2 + 1 => 255x255x64
             layers.BatchNormalization(),
-            layers.Conv2D(128, (3, 3), activation='relu', padding='valid', strides=2),
+            layers.Conv2D(128, (3, 3), activation='relu', padding='valid', strides=2), # 255x255x64 -> (255-3)/2 + 1 => 127x127x128
             layers.BatchNormalization(),
-            layers.Conv2D(256, (3, 3), activation='relu', padding='valid', strides=2),
+            layers.Conv2D(256, (3, 3), activation='relu', padding='valid', strides=2), # 127x127x128 -> (127-3)/2 + 1 => 63x63x256
             layers.BatchNormalization(),
-            layers.Conv2D(512, (3, 3), activation='relu', padding='valid', strides=2),
+            layers.Conv2D(512, (3, 3), activation='relu', padding='valid', strides=2), # 63x63x256 -> (63-3)/2 + 1 => 31x31x512
             layers.BatchNormalization(),
             layers.Flatten(),
             layers.Dense(latent_dim)
